455373d48537b41688f1f672c28f195e22cd1a60
Wilman Gonzales <wilmangonzales@rocketmail.com>
2016-08-05 10:16:25 -0400

    Add files via upload

---
+++ CPUfield/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CPUrank.Startup))]
+namespace CPUrank
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
